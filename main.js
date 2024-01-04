1594bd3e490d43cab2f19008f04d8af556055022
wfk <525816762@163.com>
2024-01-04 18:45:53 +0800

    项目初始化

---
+++ main.js
@@ -0,0 +1,45 @@
+// electron 模块可以用来控制应用的生命周期和创建原生浏览窗口
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const createWindow = () => {
+  // 创建浏览窗口
+  const mainWindow = new BrowserWindow({
+    width: 750,
+    height: 450,
+    alwaysOnTop: true,
+    x: 1500,
+    y: 100,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // 加载 index.html
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
+
+  // 打开开发工具
+  // mainWindow .webContents.toggleDevTools();
+};
+
+// 这段程序将会在 Electron 结束初始化
+// 和创建浏览器窗口的时候调用
+// 部分 API 在 ready 事件触发后才能使用。
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    // 在 macOS 系统内, 如果没有已开启的应用窗口
+    // 点击托盘图标时通常会重新创建一个新窗口
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// 除了 macOS 外，当所有窗口都被关闭的时候退出程序。
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
